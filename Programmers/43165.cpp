96d5d20533cbe5995281796339918097b4c136b1
yeen28 <48883549+yeen28@users.noreply.github.com>
2022-03-27 22:14:13 +0900

    [first] 타겟넘버

---
+++ Programmers/43165.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> gb_numbers;
+int gb_target;
+int answer = 0;
+
+void dfs(int idx, int sum){
+    if(idx == gb_numbers.size()){
+        if(sum == gb_target){
+            answer++;
+        }
+        return;
+    }
+    
+    // 더하기
+    sum += gb_numbers[idx];
+    dfs(idx+1, sum);
+    
+    // 빼기
+    sum += (-1) * (gb_numbers[idx]*2);
+    dfs(idx+1, sum);    
+}
+
+int solution(vector<int> numbers, int target) {    
+    gb_numbers = numbers;
+    gb_target = target;
+    
+    dfs(0, 0);
+    
+    return answer;
+}
