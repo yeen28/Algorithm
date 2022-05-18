3e6716776383cdd713083d2e1bd78e988a67d2ba
yeen28 <48883549+yeen28@users.noreply.github.com>
2022-05-18 10:04:17 +0900

    [second] 2920음계

--- BOJ/2920.java
+++ BOJ/2920.java
@@ -1,21 +1,46 @@
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
-  
-  public static void main(String args[]) throws Exception {
-    Scanner sc = new Scanner(System.in);
-    String scales = sc.nextLine();
+    public static void main(String args[]) throws Exception {
+        Main main = new Main();
+        main.input();
+    }
 
-    
-    if(scales.equals("1 2 3 4 5 6 7 8")) {
-      System.out.println("ascending");
-      return;
+    private void input() {
+        Scanner sc = new Scanner(System.in);
+        String scales = sc.nextLine();
+        
+        StringTokenizer stk = new StringTokenizer(scales, " ");
+        Integer[] arrScales = new Integer[8];
+
+        for (int i = 0; i < arrScales.length; i++) {
+            if (stk.hasMoreTokens()) {
+                arrScales[i] = Integer.parseInt(stk.nextToken());
+            } // end if
+        } // end for
+
+        proc(arrScales);
+    }// input
+
+    private void proc(Integer[] arrScales) {
+        Integer[] sortArrScales = new Integer[arrScales.length];
+        for(int i=0; i<arrScales.length; i++) sortArrScales[i] = arrScales[i];
+        
+        Arrays.sort(sortArrScales);
+        if(Arrays.equals(sortArrScales, arrScales)) {
+            System.out.println("ascending");
+            return;
+        }
+		
+        Arrays.sort(sortArrScales, Collections.reverseOrder());
+        if(Arrays.equals(sortArrScales, arrScales)) {
+            System.out.println("descending");
+            return;
+        }
+        
+        System.out.println("mixed");
     }
-    if(scales.equals("8 7 6 5 4 3 2 1")) {
-      System.out.println("descending");
-      return;
-    }
-    System.out.println("mixed");
-  }
 }
