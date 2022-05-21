90c23646024780eae643b6c560383c890db124eb
yeen28 <48883549+yeen28@users.noreply.github.com>
2022-05-21 13:26:59 +0900

    [second] 2475검증수

--- BOJ/2475.java
+++ BOJ/2475.java
@@ -1,33 +1,14 @@
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Main {
-	
+
 	public static void main(String args[]) throws Exception {
-		Main main = new Main();
-
-		main.input();
-	}
-
-	private void input() {
 		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		proc(str);
-	}
-	
-	private void proc(String str) {
-		int[] inputNum = new int[5];
 		int sum = 0;
-		
-		StringTokenizer stk = new StringTokenizer(str, " ");
-		for(int i=0; i<inputNum.length; i++) {
-			inputNum[i] = (int) Math.pow(Integer.parseInt(stk.nextToken()), 2);
-		} //end for
-		
-		// 제곱의 합
-		for(int i=0; i<inputNum.length; i++) {
-			sum += inputNum[i];
-		} //end for
+
+		for(int i=0; i<5; i++) {
+			sum += (int) Math.pow(sc.nextInt(), 2);
+		}
 		
 		System.out.println(sum % 10);
 	}
