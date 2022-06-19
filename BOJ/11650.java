ebab2b5b85fa166ad7b86f987d2e3e8efca3b8c2
yeen28 <48883549+yeen28@users.noreply.github.com>
2022-06-19 12:50:07 +0900

    [first] 11650좌표정렬하기

---
+++ BOJ/11650.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+	
+	public static class Coordinate {
+		int x, y;
+		
+		public Coordinate(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+    
+     public static void main(String []args) throws IOException {
+    	 Main main = new Main();
+         main.input();
+     }
+     
+     public void input() throws IOException {
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         
+         int N = Integer.parseInt(br.readLine());
+         Coordinate[] coordinate = new Coordinate[N];
+         String[] str = new String[2];
+         for(int i=0; i<N; i++) {
+        	 str = br.readLine().split(" ");
+             coordinate[i] = new Coordinate(Integer.parseInt(str[0]), Integer.parseInt(str[1]));
+         }
+         
+         proc(coordinate);
+     } //input
+     
+     public void proc(Coordinate[] coordinate) {
+         Arrays.sort(coordinate, new Comparator<Coordinate>() {
+			@Override
+			public int compare(Coordinate o1, Coordinate o2) {
+				if(o1.x == o2.x) {
+					return o1.y - o2.y;
+				}
+				
+				return o1.x - o2.x;
+			}
+         });
+         
+         for(Coordinate val : coordinate) {
+             System.out.println(val.x + " " + val.y);
+         }
+     }
+}
