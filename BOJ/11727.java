42c0f523e301e85562e95a040c6386a8e3d36354
yeeun <48883549+yeen28@users.noreply.github.com>
2022-07-19 22:54:06 +0900

    [1-1] 11727_2xn타일링2
    DP, 17808KB / 212ms

---
+++ BOJ/11727.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int[] dp = new int[1001];
+        dp[0] = 1;
+        dp[1] = 3;
+        
+        for(int i=2; i<n; i++) {
+            dp[i] = (dp[i-1] + 2*dp[i-2])%10007;
+        }
+        
+        System.out.println(dp[n-1]);
+    }
+}
