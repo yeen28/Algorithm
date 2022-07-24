0f9a022f053131e3a0b706811b9785c56d1b0c70
yeeun <48883549+yeen28@users.noreply.github.com>
2022-07-24 22:50:21 +0900

    [1-1] 10814나이순정렬
    public static class Human{ ... } 을 사용

--- BOJ/10814.java
+++ BOJ/10814.java
@@ -24,9 +24,7 @@ public class Main {
 	
 	public static void main(String args[]) throws IOException {
 		Main main = new Main();
-		
 		main.proc(main.input());
-		
 	} // main
 
 	public Human[] input() throws IOException {
