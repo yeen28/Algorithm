cd16a870447a5b8c01c318e81073f1744a0181e7
yeeun <48883549+yeen28@users.noreply.github.com>
2023-05-02 21:10:18 +0900

    [1-2] 1946신입사원
    배열 사용

--- BOJ/1946.java
+++ BOJ/1946.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-	private final Map<Integer, Integer> mapScore = new HashMap<>();
+	private int[] documentScore;
 	private int result;
 
 	public static void main(String[] args) {
@@ -33,11 +33,10 @@ public class Main {
 
 	private void proc() {
 		result = 1;
-		int rank = mapScore.get(1);
-
-		for (int i = 2; i < mapScore.size() + 1; i++) {
-			if (rank > mapScore.get(i)) {
-				rank = mapScore.get(i);
+		int rank = documentScore[1];
+		for (int i = 2; i < documentScore.length; i++) {
+			if (rank > documentScore[i]) {
+				rank = documentScore[i];
 				result++;
 			}
 		}
@@ -45,10 +44,11 @@ public class Main {
 
 	private void input(BufferedReader br) throws IOException {
 		int people = Integer.parseInt(br.readLine());
+		documentScore = new int[people + 1];
 
 		for (int i = 0; i < people; i++) {
 			StringTokenizer stk = new StringTokenizer(br.readLine());
-			mapScore.put(Integer.parseInt(stk.nextToken()), Integer.parseInt(stk.nextToken()));
+			documentScore[Integer.parseInt(stk.nextToken())] = Integer.parseInt(stk.nextToken());
 		}
 	}
 }
