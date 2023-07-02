e069668ade4e610b5e13054c60dfe5f7a5c6b4cd
yeeun <48883549+yeen28@users.noreply.github.com>
2023-07-02 10:43:18 +0900

    [1-2] 4949균형잡힌세상
    - 코드 개선
    - 문제는 괄호가 짝을 이루었는지만 체크하면 되기에 '.'으로 끝나는 문장인지 체크하는 부분 제거
    - 괄호가 짝을 이루는지 체크하는 부분을 함수로 분리
    - (, ), [, ]를 상수로 선언

--- BOJ/4949.java
+++ BOJ/4949.java
@@ -4,45 +4,56 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+	private static final char OPEN_PARENTHESIS = '(';
+	private static final char CLOSE_PARENTHESIS = ')';
+	private static final char OPEN_SQUARE_BRACKET = '[';
+	private static final char CLOSE_SQUARE_BRACKET = ']';
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String b = br.readLine();
+		String sentence = br.readLine();
 
-		while (!".".equals(b)) {
-			Stack<Character> leftBracket = new Stack<>();
-			char[] ch = b.toCharArray();
-			if ('.' != b.toCharArray()[b.length() - 1]) {
+		while (!".".equals(sentence)) { // 입력 끝 조건 : "."
+			if (checkBracketPairs(sentence)) {
+				System.out.println("yes");
+			} else {
 				System.out.println("no");
-				continue;
 			}
-			for (int i = 0; i < b.length(); i++) { // 한 문장씩 돌면서
-				if (ch[i] == '(' || ch[i] == '[') { // ch가 여는 괄호인 경우
-					leftBracket.push(ch[i]); // leftBracket에 여는 괄호만 push
-				} else if (ch[i] == ')') {
-					if (leftBracket.isEmpty() || leftBracket.peek() != '(') {
-						System.out.println("no"); // ch가 )가 아니면 결과는 NO
-						break;
-					} else {
-						// ch가 )인 경우 stack top은 (이어야 pop 가능
-						leftBracket.pop();
-					}
-				} else if (ch[i] == ']') {
-					if (leftBracket.isEmpty() || leftBracket.peek() != '[') {
-						System.out.println("no"); // ch가 ]가 아니면 결과는 NO
-						break;
-					} else {
-						// ch가 ]인 경우 stack top은 [이어야 pop 가능
-						leftBracket.pop();
-					}
-				} else if (i == b.length() - 1) {
-					if (leftBracket.isEmpty()) {
-						System.out.println("yes");
-					} else {
-						System.out.println("no");
-					}
-				}
-			}
-			b = br.readLine();
+
+			sentence = br.readLine();
 		}
+	}
+
+	private static boolean checkBracketPairs(String sentence) {
+		Deque<Character> leftBrackets = new ArrayDeque<>();
+
+		for (int i = 0; i < sentence.length(); i++) { // 문장 안의 문자들을 돌면서
+			char ch = sentence.charAt(i);
+
+			if (ch == OPEN_PARENTHESIS || ch == OPEN_SQUARE_BRACKET) { // ch가 여는 괄호인 경우
+				leftBrackets.push(ch); // leftBrackets에 여는 괄호만 push
+
+			} else if (ch == CLOSE_PARENTHESIS) {
+				if (leftBrackets.isEmpty() || leftBrackets.peek() != OPEN_PARENTHESIS) {
+					return false; // ch가 )가 아니면 결과는 NO
+				} else {
+					// ch가 )인 경우 stack top은 (이어야 pop 가능
+					leftBrackets.pop();
+				}
+
+			} else if (ch == CLOSE_SQUARE_BRACKET) {
+				if (leftBrackets.isEmpty() || leftBrackets.peek() != OPEN_SQUARE_BRACKET) {
+					return false; // ch가 ]가 아니면 결과는 NO
+				} else {
+					// ch가 ]인 경우 stack top은 [이어야 pop 가능
+					leftBrackets.pop();
+				}
+
+			} else if (i == sentence.length() - 1) {
+				return leftBrackets.isEmpty();
+			}
+		}
+
+		return false;
 	}
 }
