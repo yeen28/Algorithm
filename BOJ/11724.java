ab2ebfecdb8066ea56d7330d6008395641697edf
yeeun <48883549+yeen28@users.noreply.github.com>
2023-08-22 20:33:36 +0900

    [1-1] 11724연결요소의개수

---
+++ BOJ/11724.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	private int[][] nodes;
+	private boolean[] visited;
+	private int vertex;
+	private int result;
+
+	public static void main(String[] args) throws IOException {
+		Main main = new Main();
+
+		main.input();
+		main.proc();
+	}
+
+	public void dfs(int i) {
+		visited[i] = true;
+
+		for (int j = 0; j <= vertex; j++) {
+			if (visited[j] || nodes[i][j] == 0) {
+				continue;
+			}
+
+			dfs(j);
+		}
+	}
+
+	public void proc() {
+		for (int i = 1; i <= vertex; i++) {
+			if (!visited[i]) {
+				result++;
+			}
+
+			dfs(i);
+		}
+
+		System.out.println(result);
+	}
+
+	public void input() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk = new StringTokenizer(br.readLine());
+		vertex = Integer.parseInt(stk.nextToken());
+		int edge = Integer.parseInt(stk.nextToken());
+
+		nodes = new int[vertex + 1][vertex + 1];
+		visited = new boolean[vertex + 1];
+
+		for (int i = 0; i < edge; i++) {
+			stk = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(stk.nextToken());
+			int b = Integer.parseInt(stk.nextToken());
+			nodes[a][b] = nodes[b][a] = 1;
+		}
+	}
+}
