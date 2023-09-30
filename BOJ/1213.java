6de628e70c8460af78b4092613364a506c9b1cd1
yeeun <48883549+yeen28@users.noreply.github.com>
2023-10-01 00:17:00 +0900

    [1-1] 1213팰린드롬만들기

--- BOJ/1213.java
+++ BOJ/1213.java
@@ -1,3 +1,5 @@
+// https://y-e-un28.tistory.com/520
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,13 +14,16 @@ public class Main {
 	}
 
 	public void proc() {
+		StringBuilder before = new StringBuilder();
+		StringBuilder after;
 		StringBuilder result = new StringBuilder();
 		int odd = 0;
 		int mid = -1;
 		int[] alphabet = new int[26];
 
-		for (int i = 0; i < name.length; i++) {
-			alphabet[name[i] - 65]++;
+		// 각 문자의 개수를 저장
+		for (char c : name) {
+			alphabet[c - 65]++;
 		}
 
 		for (int i = 0; i < alphabet.length; i++) {
@@ -27,25 +32,20 @@ public class Main {
 				mid = i;
 			}
 
-			if (odd > 1) {
+			if (odd > 1) { // 팰린드롬을 만들 수 없는 경우
 				System.out.println("I'm Sorry Hansoo");
 				return;
 			}
 		}
 
+		// 팰린드롬 앞 부분 만들기
 		for (int i = 0; i < alphabet.length; i++) {
 			alphabet[i] /= 2;
-
-			for (int j = 0; j < alphabet[i]; j++) {
-				result.append((char)(i + 'A'));
-			}
+			before.append(String.valueOf((char) (i + 'A')).repeat(Math.max(0, alphabet[i])));
 		}
-
-		StringBuilder after = new StringBuilder();
-		for (int i = 1; i <= result.length(); i++) {
-			char[] charArray = result.toString().toCharArray();
-			after.append(charArray[charArray.length - i]);
-		}
+		
+		result.append(before);
+		after = before.reverse();
 
 		if (mid != -1) {
 			result.append((char) (mid + 'A'));
