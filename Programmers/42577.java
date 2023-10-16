61467b76519fedee91ea21038de4352081aa3d7a
yeeun <48883549+yeen28@users.noreply.github.com>
2023-10-16 21:16:42 +0900

    [1-1] 전화번호 목록

---
+++ Programmers/42577.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+	public boolean solution(String[] phone_book) {
+ 		if (phone_book.length == 1) {
+			return false;
+		}
+
+		Map<String, Integer> map = new HashMap<>();
+		int idx = 0;
+		for (String str : phone_book) {
+			map.put(str, idx++);
+		}
+
+		for (String str : phone_book) {
+			for (int j = 0; j < str.length(); j++) {
+				if (map.containsKey(str.substring(0, j))) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+}
