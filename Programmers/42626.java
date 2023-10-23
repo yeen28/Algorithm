16ab1d70f8f8ca873692e3cb0eeb0e177db7a3f4
yeeun <48883549+yeen28@users.noreply.github.com>
2023-10-24 05:39:45 +0900

    [1-1] 더맵게

---
+++ Programmers/42626.java
@@ -0,0 +1,23 @@
+import java.util.PriorityQueue;
+
+class Solution {
+	public int solution(int[] scoville, int K) {
+		int answer = 0;
+		PriorityQueue<Integer> que = new PriorityQueue<>();
+
+		for (int num : scoville) {
+			que.add(num);
+		}
+
+		while (que.peek() < K) {
+			if (que.size() < 2) {
+				return -1;
+			}
+
+			que.add(que.poll() + que.poll() * 2);
+			answer++;
+		}
+
+		return answer;
+	}
+}
