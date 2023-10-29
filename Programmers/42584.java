30c76af6ff1131d80d9d0fa8965faa684673e56f
yeeun <48883549+yeen28@users.noreply.github.com>
2023-10-29 14:50:27 +0900

    [1-1] 주식가격

---
+++ Programmers/42584.java
@@ -0,0 +1,20 @@
+// https://y-e-un28.tistory.com/540
+
+class Solution {
+	public int[] solution(int[] prices) {
+		int[] answer = new int[prices.length];
+
+		for (int i = 0; i < prices.length - 1; i++) {
+			int cnt = 0;
+			for (int j = i + 1; j < prices.length; j++) {
+				cnt++;
+				if (prices[i] > prices[j]) {
+					break;
+				}
+			}
+			answer[i] = cnt;
+		}
+
+		return answer;
+	}
+}
