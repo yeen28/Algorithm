9bf2a58de6ed70e9806f7ae80801e043d0f68d69
yeeun <48883549+yeen28@users.noreply.github.com>
2025-05-18 12:02:41 +0900

    [Gold V] Title: Z, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.java
+++ 백준/Gold/1074. Z/Z.java
@@ -14,44 +14,41 @@ public class Main {
     
     public void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer stk = new StringTokenizer(br.readLine());
         
         N = Integer.parseInt(stk.nextToken());
+
         // r행 c열
         r = Integer.parseInt(stk.nextToken());
         c = Integer.parseInt(stk.nextToken());
     }
     
     public void proc() {
-        int y = (int)Math.pow(2, N) / 2;
-        int x = y;
+        int x = (int)Math.pow(2, N - 1);
+        int y = x;
         int answer = 0;
         
         while(N-- > 0) { //비교 후 감소
-            int tmp = (int)Math.pow(2, N) / 2;
+            int tmp = (int)Math.pow(2, N - 1);
             int skip = (int)Math.pow(4, N);
             
-            if(r < y && c < x) { 
-                //1구역
+            if(r < y && c < x) { // 1구역
                 x -= tmp;
                 y -= tmp;
-            } else if(r < y && c >= x) { 
-                //2구역
+            } else if(r < y && c >= x) { // 2구역
                 x += tmp;
                 y -= tmp;
                 answer += skip;
-            } else if(r >= y && c < x) { 
-                //3구역
+            } else if(r >= y && c < x) { // 3구역
                 x -= tmp;
                 y += tmp;
-                answer += skip*2;
-            } else { 
-                //4구역
+                answer += skip * 2;
+            } else { // 4구역
                 x += tmp;
                 y += tmp;
-                answer += skip*3;
+                answer += skip * 3;
             }
-        } //end while
+        }
         
         System.out.println(answer);
     }
