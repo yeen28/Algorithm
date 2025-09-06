2de312d26e1aba13f15d10f4e9cdd592319f3962
yeeun <48883549+yeen28@users.noreply.github.com>
2025-09-06 20:57:45 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.03 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,53 +1,12 @@
 class Solution {
     public int solution(int n, int w, int num) {
-        int answer = -1;
-        int[][] boxMap = loadBox(n, w);
-        
-        for (int i = 0; i < boxMap.length; i++) {
-            for (int j = 0; j < boxMap[i].length; j++) {
-                if (boxMap[i][j] == num) {
-                    if (boxMap[0][j] == 0) {
-                        return i;
-                    } else {
-                        return i + 1;
-                    }
-                }
-            }
+        int cnt = 0;
+        while(num <= n) {
+            num += (w - ((num-1) % w) -1) * 2 + 1;
+            cnt++;
         }
-        
+
+        int answer = cnt;
         return answer;
-    }
-    
-    /**
-     * 박스 쌓기
-     */
-    private int[][] loadBox(int n, int w) {
-        int layer = n / w + 1;
-        int[][] boxMap = new int[layer][w];
-        int boxIdx = 1;
-        
-        for (int i = layer - 1; i >= 0; i--) {
-            if (i % 2 != 0) {
-                for (int j = 0; j < w; j++) {
-                    if (boxIdx > n) {
-                        return boxMap;
-                    }
-                    
-                    boxMap[i][j] = boxIdx;
-                    boxIdx++;
-                }
-            } else {
-                for (int j = w - 1; j >= 0; j--) {
-                    if (boxIdx > n) {
-                        return boxMap;
-                    }
-                    
-                    boxMap[i][j] = boxIdx;
-                    boxIdx++;
-                }
-            }
-        }
-        
-        return boxMap;
     }
 }
